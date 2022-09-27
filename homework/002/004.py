64db4dfd572a2af60b0ee85c5bd03eab5d031499
Yury <pakulin.yury@gmail.com>
2022-09-27 21:02:47 +0300

    фикс

--- homework/002/004.py
+++ homework/002/004.py
@@ -4,10 +4,17 @@
 #Number of elements: 5
 #-> [-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5]
 #-> 15
-pos1=int(input('Позиция 1: '))
-pos2=int(input('Позиция 2: '))
-limits=int(input('Число элементов: '))
+while True:
+    limits=int(input('Число элементов: '))
+    if limits>0:
+        break
 length=2*limits+1
+while True:
+    print('Введите значения от 1 по ', length)
+    pos1=int(input('Позиция 1: '))
+    pos2=int(input('Позиция 2: '))
+    if (0<pos1<length+1) and (0<pos2<length+1):
+        break
 numbers=[0]*length
 for i in range(length):
     numbers[i]=-limits+i
