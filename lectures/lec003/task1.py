d87aef8817982f87aea136dad856b76fc9bb18fd
lewe0fun <pakuln.yury@gmail.com>
2022-10-06 17:06:54 +0300

    3 лекция

--- lectures/lec003/task1.py
+++ lectures/lec003/task1.py
@@ -1,11 +1,19 @@
 
-path='test/data.txt'
+path='lectures/lec003/data.txt'
 data=open(path,'r')
-arr=data.readline().split(' ')
+# arr=data.readline().split(' ')
+# data.close()
+# result=[]
+# for i in range(len(arr)):
+#     if int(arr[i])%2==0:
+#         temp=(int(arr[i]),int(arr[i])**2)
+#         result.append(temp)
+# print(result)
+arr=list(map(int,(data.readline().split(' '))))
 data.close()
 result=[]
 for i in range(len(arr)):
-    if int(arr[i])%2==0:
-        temp=(int(arr[i]),int(arr[i])**2)
+    if arr[i]%2==0:
+        temp=((arr[i]),(arr[i])**2)
         result.append(temp)
 print(result)
