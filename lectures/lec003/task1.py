7964f7b005c93a822c2f27247584d98f5fd26c56
lewe0fun <pakuln.yury@gmail.com>
2022-10-06 17:31:06 +0300

    3 лекция

--- lectures/lec003/task1.py
+++ lectures/lec003/task1.py
@@ -1,19 +1,19 @@
 
 path='lectures/lec003/data.txt'
 data=open(path,'r')
-# arr=data.readline().split(' ')
-# data.close()
-# result=[]
-# for i in range(len(arr)):
-#     if int(arr[i])%2==0:
-#         temp=(int(arr[i]),int(arr[i])**2)
-#         result.append(temp)
-# print(result)
-arr=list(map(int,(data.readline().split(' '))))
+
+arr=data.readline().split(' ')
 data.close()
 result=[]
 for i in range(len(arr)):
-    if arr[i]%2==0:
-        temp=((arr[i]),(arr[i])**2)
+    if int(arr[i])%2==0:
+        temp=(int(arr[i]),int(arr[i])**2)
         result.append(temp)
-print(result)
+print(result,'Через итерациию:')
+
+# через map и list complehentions
+data=open(path,'r')
+arr2=list(map(int,(data.readline().split(' '))))
+data.close()
+result2=[(arr2[i], arr2[i]**2) for i in range(len(arr2)) if arr2[i]%2==0]
+print(result2,'Через map и list complehentions')
